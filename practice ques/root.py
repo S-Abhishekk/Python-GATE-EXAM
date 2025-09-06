677fbbeb7b5343a7787d807777a82775808e3056
Abhishek Singh <167851013+S-Abhishekk@users.noreply.github.com>
2025-09-06 13:03:11 +0000

    root

---
+++ practice ques/root.py
@@ -0,0 +1,8 @@
+#a = int(input("Enter the number:"))
+#b= a**(1/2)
+#print("The square root of the number is :", b)
+
+import math
+a = int(input("Enter the number:"))
+b = math.sqrt(a)
+print("The square root of the number is :", b)
