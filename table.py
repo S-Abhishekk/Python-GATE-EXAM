8cef867ced1b41341805fde4e8bc5869b6f63c9a
Abhishek Singh <167851013+S-Abhishekk@users.noreply.github.com>
2025-09-09 15:19:21 +0000

    tables

---
+++ table.py
@@ -0,0 +1,11 @@
+"""num = int(input("Enter the number: "))
+
+for i in range(0,11):
+    print(num, "X", i, "=", num*i )"""
+
+#While loop
+n = int(input("Enter the number: "))
+i = 0
+while i<=10:
+    print(n, "X", i, "=", n*i)
+    i = i+1
